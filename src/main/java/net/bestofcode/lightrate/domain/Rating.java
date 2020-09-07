ef0dc276705088febf425f22ac7561804adc0b9d
maste150hhu <mastremolten2@gmail.com>
2020-09-07 16:51:36 +0200

    implement constructor for Rating

--- src/main/java/net/bestofcode/lightrate/domain/Rating.java
+++ src/main/java/net/bestofcode/lightrate/domain/Rating.java
@@ -7,10 +7,14 @@ public class Rating {
     private final Title title;
     private final Description description;
     private final RatingID ratingID;
+    private Integer count;
+    private Double rating;
 
     public Rating(Title title, Description description) {
         this.title = title;
         this.description = description;
         this.ratingID = new RatingID(UUID.randomUUID());
+        this.count = 0;
+        this.rating = -1.0;
     }
 }
