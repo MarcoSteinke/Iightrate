7576d89ea61bf63e2616c5fb93a6c8793411a159
maste150hhu <mastremolten2@gmail.com>
2020-09-07 15:51:01 +0200

    add spring project

---
+++ src/main/java/net/bestofcode/lightrate/LightrateApplication.java
@@ -0,0 +1,13 @@
+package net.bestofcode.lightrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightrateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightrateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/bestofcode/lightrate/LightrateApplicationTests.java
@@ -0,0 +1,13 @@
+package net.bestofcode.lightrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightrateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
