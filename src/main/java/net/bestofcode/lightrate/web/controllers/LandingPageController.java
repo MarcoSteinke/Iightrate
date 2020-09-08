33bd207f710acfa78a5138ce55322a68b9c1aee2
maste150hhu <mastremolten2@gmail.com>
2020-09-08 18:50:55 +0200

    add default behaviour to LandingPageController

--- src/main/java/net/bestofcode/lightrate/web/controllers/LandingPageController.java
+++ src/main/java/net/bestofcode/lightrate/web/controllers/LandingPageController.java
@@ -1,4 +1,14 @@
 package net.bestofcode.lightrate.web.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LandingPageController {
+
+    @GetMapping("/")
+    public String returnLandingPage() {
+        return "index";
+    }
 }
