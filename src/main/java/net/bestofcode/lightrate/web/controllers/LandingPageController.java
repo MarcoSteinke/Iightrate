6bf6ce5a8d2a661afb725fc95187e0e977c7686a
maste150hhu <mastremolten2@gmail.com>
2020-09-09 12:06:26 +0200

    integrate workflow

--- src/main/java/net/bestofcode/lightrate/web/controllers/LandingPageController.java
+++ src/main/java/net/bestofcode/lightrate/web/controllers/LandingPageController.java
@@ -1,5 +1,10 @@
 package net.bestofcode.lightrate.web.controllers;
 
+import net.bestofcode.lightrate.domain.model.Description;
+import net.bestofcode.lightrate.domain.model.Rating;
+import net.bestofcode.lightrate.domain.model.Title;
+import net.bestofcode.lightrate.domain.services.DatabaseService;
+import net.bestofcode.lightrate.domain.services.RatingService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +14,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LandingPageController {
 
+    DatabaseService databaseService;
+    RatingService ratingService;
+
+    public LandingPageController(DatabaseService databaseService, RatingService ratingService) {
+        this.databaseService = databaseService;
+        this.ratingService = ratingService;
+    }
+
     @GetMapping("/")
     public String returnLandingPage() {
         return "index";
@@ -17,8 +30,13 @@ public class LandingPageController {
     @PostMapping("/")
     public String createNewRating(Model model, @RequestParam String title, @RequestParam String description) {
 
-        model.addAttribute("title", title);
-        model.addAttribute("description", description);
+        // validate and persist the information
+        Rating rating = this.ratingService.createRating(
+                new Title(title),
+                new Description(description)
+        );
+
+        model.addAttribute("rating", rating);
         return "rating";
     }
 }
